f8d6ed8c1775cddc187d6ace3e690bd6390ff227
imsahiljain <emailofsahiljain@gmail.com>
2023-04-14 15:16:54 +0530

    updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,10 +15,14 @@ function App({ Component, pageProps }) {
       <Head>
         <link rel="icon" href="/pfp.jpg" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="default-src 'self' vitals.vercel-insights.com"
+        />
         <meta name="author" content="Sahil Jain" />
         <meta
           name="description"
-          content="Hi, I'm Sahil, a developer, basketball player and an artist. I'm a 16 y/o high school senior at Montfort School in Delhi."
+          content="Hi, I'm Sahil, a developer, basketball player and an artist. I'm a 17 y/o high school senior at Montfort School in Delhi."
         />
         <meta name="keywords" content="Sahil, Sahil Jain, imsahil.co" />
         <link rel="apple-touch-icon" href="/pfp.jpg" />
